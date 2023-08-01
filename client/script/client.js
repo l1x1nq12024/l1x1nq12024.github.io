c1b6a69c782943e981aa288a08cfcbf5324ad41d
l1x1nq12024 <134377853+l1x1nq12024@users.noreply.github.com>
2023-08-01 21:53:53 +0800

    Add files via upload

---
+++ client/script/client.js
@@ -0,0 +1,44 @@
+var Client={};
+Client.socket=io();
+Client.AskNewPlayer=function(){
+    Client.socket.emit('newplayer');
+};
+Client.socket.on('newplayer',function(data){
+    AddNewPlayer(data.id,data.x,data.y);
+});
+Client.socket.on('allplayers',function(data){
+    console.log(data);
+    for(var i=0;i<data.length;i++){
+        AddNewPlayer(data[i].id,data[i].x,data[i].y);
+    }
+});
+Client.socket.on('removeplayer',function(id){
+    RemovePlayer(id);
+});
+Client.socket.on('yourid',function(id){
+    mineid=id;
+    if(mineid==1)isdev=1;
+});
+Client.SendKey=function(n,key){
+    Client.socket.emit('sendkey',n,key);
+}
+Client.socket.on('keyevent',function(id,n,key){
+    // console.log(id+':'+n+' '+key);
+    keys[id][key]=n;
+});
+Client.Update=function(id,p){
+    var obj={
+        x:p.x,
+        y:p.y,
+        an:p.anims.currentAnim.key,
+        hx:p.hand.x,
+        hy:p.hand.y,
+        hpx:p.hps.x,
+        hpy:p.hps.y,
+        hpsx:p.hps.scaleX
+    };
+    Client.socket.emit('update',id,JSON.stringify(obj));
+};
+Client.socket.on('setnew',function(id,str){
+    SetNew(id,str);
+});
